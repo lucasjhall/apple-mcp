2a1b9593b4c7ad5c4e438b5b6587ac76efb28da4
Lucas Hall <lucasjhall@users.noreply.github.com>
2025-08-16 13:57:08 -0400

    Initial Commit

---
+++ apple-mcp/apple_mcpApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_mcpApp.swift
+//  apple-mcp
+//
+//  Created by lucas on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct apple_mcpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
